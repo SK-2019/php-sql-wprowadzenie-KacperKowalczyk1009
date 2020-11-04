03eef4ebeffb1f204739daeb0f48e1a43c062bca
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-04 14:55:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <?php
    require_once('conn.php');
 
-    $sql=('SELECT * FROM pracownicy');
+    $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
     $result=$conn->query($sql);
         echo("<hr />");
         echo("<h3>Tabela Pracowników</h3>");
@@ -20,9 +20,10 @@
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
+        echo("<th>nazwa_dzial</th>");  
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki'].".$row['nazwa_dzial']."</td>");
                 echo("</tr>");
             }
         echo("</table>");

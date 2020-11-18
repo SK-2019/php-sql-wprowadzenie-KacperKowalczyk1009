98fef79a5cd531d9973b7425612b9f9333d4b9ea
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-18 14:47:15 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -2,8 +2,18 @@
 
 echo("jesteś na strona.php");
 
-echo($_POST["imie"]);
-echo($_POST["Nazwisko"]);
-echo($_POST["Klasa"]);
+echo("<li>imię:".$_POST["imie"]);
+echo("<li>nazwisko:".$_POST["nazwisko"]);
+echo("<li>klasa:".$_POST["klasa"]);
 
+require_once('conn.php');
+  $sql = "INSERT INTO pracownicy (imie, nazwisko)
+  VALUES ('John', 'Doe')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
 ?>

31ece8341f86ec69824eeda3461209af5e96046d
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-20 08:36:59 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -22,10 +22,13 @@
 <?php
 
 echo("jesteś na strona.php");
-
+     
+echo("<li>id_pracownicy:".$_POST["id_pracownicy"]);
 echo("<li>imię:".$_POST["imie"]);
 echo("<li>nazwisko:".$_POST["nazwisko"]);
-echo("<li>klasa:".$_POST["klasa"]);
+echo("<li>dzial:".$_POST["dzial"]);
+echo("<li>zarobki:".$_POST["zarobki"]);
+echo("<li>data_urodzenia:".$_POST["data_urodzenia"]);     
 
 require_once('conn.php');
   $sql = "INSERT INTO `pracownicy`('id_pracownicy', 'imie', 'nazwisko', 'dzial', 'zarobki', 'data_urodzenia') VALUES (null, 'Kamil', 'Bonk', 3, 33, 1991-01-11)";

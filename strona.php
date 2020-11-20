a30d8c6ce852c46ea107aafd3c89d69e31c8673c
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-20 09:05:40 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -25,13 +25,12 @@ echo("jesteś na strona.php");
      
 echo("<li>id_pracownicy:".$_POST["id_pracownicy"]);
 echo("<li>imię:".$_POST["imie"]);
-echo("<li>nazwisko:".$_POST["nazwisko"]);
 echo("<li>dzial:".$_POST["dzial"]);
 echo("<li>zarobki:".$_POST["zarobki"]);
 echo("<li>data_urodzenia:".$_POST["data_urodzenia"]);     
 
 require_once('conn.php');
-  $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `nazwisko`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['nazwisko']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_urodzenia']."')";
+  $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_urodzenia']."')";
   
   
   if ($conn->query($sql) === TRUE) {

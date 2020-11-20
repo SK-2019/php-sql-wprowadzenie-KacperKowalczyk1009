f465a1b4716b1488eb136eb661de70f6903efa90
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-20 09:21:34 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -7,10 +7,11 @@
 
 <h1>Kacper Kowalczyk 2Ti nr 16</h1>
 <a href="https://github.com/SK-2019/php-sql-wprowadzenie-KacperKowalczyk1009"> Mój GITHUB </a>
-   <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
+   <<a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
    <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
    <a class="nav_link" href="/">Strona Glowna</a>  
+   <a class="nav_link" href="formularz.html"><b>Formularz<b></a>
    
    <?php
 

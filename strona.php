372e83ac2c2ff91aded1d9ed00cd009975d38b5b
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-20 08:16:43 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,3 +1,24 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Kacper Kowalczyk 2Ti nr 16</h1>
+  
+  
+  
+   <a href="https://github.com/SK-2019/php-sql-wprowadzenie-KacperKowalczyk1009"> Mój GITHUB </a>
+  
+  <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
+   <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
+   <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
+   <a class="nav_link" href="/">Strona Glowna</a>  
+   <a class="nav_link" href="formularz.html"><b>Formularz<b></a>
+  
+
+
+
+
 <?php
 
 echo("jesteś na strona.php");
@@ -17,3 +38,7 @@ require_once('conn.php');
   }
   $conn->close();
 ?>
+     
+     </body>
+</head>
+</html>

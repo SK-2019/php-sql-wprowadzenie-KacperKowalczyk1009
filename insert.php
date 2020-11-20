a13faed520788c743793be73dec091a223f1aa77
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-20 13:01:24 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,14 +2,13 @@
 
 echo("jesteś na strona.php");
      
-echo("<li>id_pracownicy:".$_POST["id_pracownicy"]);
-echo("<li>imię:".$_POST["imie"]);
+echo("<li>name:".$_POST["name"]);
 echo("<li>dzial:".$_POST["dzial"]);
 echo("<li>zarobki:".$_POST["zarobki"]);
 echo("<li>data_urodzenia:".$_POST["data_urodzenia"]);     
 
 require_once('conn.php');
-  $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_urodzenia']."')";
+  $sql = "INSERT INTO pracownik(`name`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['name']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_urodzenia']."')";
   
   
   if ($conn->query($sql) === TRUE) {

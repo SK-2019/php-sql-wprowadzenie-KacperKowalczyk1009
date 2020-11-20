6f730752e6190d9bbe90123c470f106b92a14399
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-20 12:57:21 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,4 +1,21 @@
 <?php
-echo("jestes w insert.php");
-echo $_POST['name'];
+
+echo("jesteś na strona.php");
+     
+echo("<li>id_pracownicy:".$_POST["id_pracownicy"]);
+echo("<li>imię:".$_POST["imie"]);
+echo("<li>dzial:".$_POST["dzial"]);
+echo("<li>zarobki:".$_POST["zarobki"]);
+echo("<li>data_urodzenia:".$_POST["data_urodzenia"]);     
+
+require_once('conn.php');
+  $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_urodzenia']."')";
+  
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
 ?>

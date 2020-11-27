d12ad3fc81c88bf203917eff69f457fd641d030f
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-11-27 13:28:53 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -12,7 +12,7 @@
    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
    <a class="nav_link" href="/">Strona Glowna</a>  
    <a class="nav_link" href="formularz.html"><b>Formularz<b></a> 
-   <a class="nav_link" href="daneDoBazy.html"><b>Dane do bazy<b></a> 
+   <a class="nav_link" href="daneDoBazy.php"><b>Dane do bazy<b></a> 
       <a class="nav_link" href="function.php"><b>Function<b></a>
 
 <?php

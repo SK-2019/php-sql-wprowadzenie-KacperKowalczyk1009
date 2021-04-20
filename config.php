a189167efa3bb3cfbd4637d429f8706f68c2e90b
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2021-04-20 08:28:07 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$_SERVER['pass'] = 'nieznanehaslo_local'
+
+$_SERVER['servername'] = "remotemysql.com";
+$_SERVER['username'] = "Qp5kr9aFAZ";
+$_SERVER['password'] = "nLj0fYkQ76";
+$_SERVER['dbname'] = "Qp5kr9aFAZ";
+?>

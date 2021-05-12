17c619313525fe0457c9049006210d9a5037c0d7
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2021-05-12 23:43:24 +0200

    Add files via upload

---
+++ flexbox/flexbox.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+
+
+
+        <head>
+        <link rel="stylesheet" href="/assets/styleflexbox.css">
+        <link rel="shortcut icon" href="/inne/favicon.ico">
+        </head>
+
+        <div class="nav">
+        <a class="nav_link" href="/">Strona Glowna</a>  
+            </div>
+
+        <body>
+            <div class="con">
+                <div class="item a">
+                <div class="prostokat">
+                    <div class="menu">
+                        <a class="pierwsze" href="">ak47</a>
+                        <a class="pierwsze" href="">m4a4</a>
+                        <a class="pierwsze" href="">aug</a>
+                        <a class="pierwsze" href="">ssg</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item b">
+                <div class="prostokat">
+                    <div class="menu">
+                    <a class="drugie" href="">p90</a>
+                    <a class="drugie" href="">ump-47</a>
+                    <a class="drugie" href="">usp-s</a>
+                    <a class="drugie" href="">glock-18</a>
+                </div>
+            </div>
+        </div>
+        <div class="item c">
+            <div class="prostokat">
+                <div class="menu2">
+                <a class="trzecie" href="">p250</a>
+                <a class="trzecie" href="">five-seven</a>
+                <a class="trzecie" href="">negev</a>
+                <a class="trzecie" href="">m249</a>
+            </div>
+        </div>
+    </div>
+    <div class="item d">
+        <div class="prostokat">
+            <div class="menu2">
+            <a class="czwarte" href="">mac-10</a>
+            <a class="czwarte" href="">mp9</a>
+            <a class="czwarte" href="">deagle</a>
+            <a class="czwarte" href="">p2000</a>
+        </div>
+    </div>
+</div>
+<div class="item e">
+    <div class="prostokat"></div>
+</div>
+<div class="item f">
+    <div class="prostokat"></div>
+</div>
+<div class="item g">
+    <div class="prostokat"></div>
+</div>
+<div class="item h">
+    <div class="kwadrat"></div>
+    <div class="kwadrat"></div>
+</div>
+<div class="item i">
+    <div class="kwadrat"></div>
+    <div class="kwadrat"></div>
+</div>
+<div class="item j">
+    <div class="kwadrat"></div>
+    <div class="kwadrat"></div>
+</div>
+<div class="item k">
+    <div class="kwadrat"></div>
+    <div class="kwadrat"></div>
+</div>
+</div>
+</body>
+</html>
